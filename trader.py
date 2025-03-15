59af7cd77f59a6bb7932000a8336e122e152fd66
Aleksandar Shokolarov <akshokolarov@gmail.com>
2025-03-15 17:19:59 +0100

    malta

--- trader.py
+++ trader.py
@@ -1,40 +1,66 @@
+from abc import ABC, abstractmethod
 from typing import List
 
 from datamodel import Order, OrderDepth, TradingState
+
+
+class Product(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def calculate_orders(self, order_depth):
+        pass
+
+
+class RainforestResin(Product):
+    def __init__(self, trigger_params):
+        self.name = "Rainforest Resin"
+        self.symbol = "RAINFOREST_RESIN"
+        self._max_position = 50
+        self._min_position = -50
+        self._RESIN_MEAN = 1e4
+        self._RESIN_STD = 1.48
+        self._x, self._y = trigger_params
+
+    def calculate_orders(self, order_depth, position=None):
+        moves = []
+
+        best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+        best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+
+        price = (best_ask + best_bid) / 2
+
+        if price >= int(self._RESIN_MEAN + self._x * self._RESIN_STD):
+            ask_price = best_bid + self._y
+            ask_volume = 1
+            bid_order = Order(self.symbol, ask_price, -ask_volume)
+            moves.append(bid_order)
+        elif price <= int(self._RESIN_MEAN - self._x * self._RESIN_STD):
+            bid_price = best_ask - self._y
+            bid_volume = 1
+            ask_order = Order(self.symbol, bid_price, bid_volume)
+            moves.append(ask_order)
+        return moves
+
+
+products = {"RAINFOREST_RESIN": RainforestResin((2, 1))}
 
 
 class Trader:
     def run(self, state: TradingState):
         # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
         print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
+
         result = {}
         for product in state.order_depths:
-            order_depth: OrderDepth = state.order_depths[product]
-            orders: List[Order] = []
-            acceptable_price = 10  # Participant should calculate this value
-            print("Acceptable price : " + str(acceptable_price))
-            print(
-                "Buy Order depth : "
-                + str(len(order_depth.buy_orders))
-                + ", Sell order depth : "
-                + str(len(order_depth.sell_orders))
-            )
-
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
-
-            # result[product] = orders
-            result[product] = []
+            if product in products.keys():
+                order_depth = state.order_depths[product]
+                orders = products[product].calculate_orders(order_depth)
+            else:
+                orders = []
+            result[product] = orders
 
         traderData = "SAMPLE"  # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
 
