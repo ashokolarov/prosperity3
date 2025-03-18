4abb345726aa45c033584143393d0c147e099204
Aleksandar Shokolarov <ashokolarov@SyneAlexs-MacBook-Pro.local>
2025-03-18 18:23:58 +0100

    closing orders now

---
+++ market_utils.py
@@ -0,0 +1,193 @@
+class OrderBook:
+    def __init__(self):
+        self.ask_prices = []
+        self.ask_volumes = []
+        self.bid_prices = []
+        self.bid_volumes = []
+
+        self._prev_ask_prices = []
+        self._prev_ask_volumes = []
+        self._prev_bid_prices = []
+        self._prev_bid_volumes = []
+
+    def reset(self, order_depths):
+        sell_orders = order_depths.sell_orders
+        buy_orders = order_depths.buy_orders
+
+        sell_orders = list(sell_orders.items())
+        self.ask_prices = [order[0] for order in sell_orders]
+        self.ask_volumes = [abs(order[1]) for order in sell_orders]
+
+        buy_orders = list(buy_orders.items())
+        self.bid_prices = [order[0] for order in buy_orders]
+        self.bid_volumes = [abs(order[1]) for order in buy_orders]
+
+    def get_ask_order_at_depth(self, depth):
+        return self.ask_prices[depth], self.ask_volumes[depth]
+
+    def get_bid_order_at_depth(self, depth):
+        return self.bid_prices[depth], self.bid_volumes[depth]
+
+    @property
+    def bid_orders_depth(self):
+        return len(self.bid_prices)
+
+    @property
+    def ask_orders_depth(self):
+        return len(self.ask_prices)
+
+    @property
+    def best_ask(self):
+        return self.ask_prices[0], self.ask_volumes[0]
+
+    @property
+    def best_bid(self):
+        return self.bid_prices[0], self.bid_volumes[0]
+
+    @property
+    def spread(self):
+        return self.ask_prices[0] - self.bid_prices[0]
+
+    @property
+    def mid_price(self):
+        return (self.ask_prices[0] + self.bid_prices[0]) / 2
+
+    @property
+    def vwap(self):
+        buy_vwap = sum(
+            [price * volume for price, volume in zip(self.bid_prices, self.bid_volumes)]
+        ) / sum(self.bid_volumes)
+        sell_vwap = sum(
+            [price * volume for price, volume in zip(self.ask_prices, self.ask_volumes)]
+        ) / sum(self.ask_volumes)
+        mid_vwap = (buy_vwap + sell_vwap) / 2
+        return buy_vwap, sell_vwap, mid_vwap
+
+    def __repr__(self):
+        repr_str = "BID ORDER PRICE | VOLUME | ASK ORDER PRICE\n"
+        length = max(self.ask_prices) - min(self.bid_prices)
+        lines = [repr_str]
+        for i in range(length + 1):
+            price_level = max(self.ask_prices) - i
+            if (
+                price_level not in self.ask_prices
+                and price_level not in self.bid_prices
+            ):
+                continue
+            bid_line = (
+                "               "
+                if price_level not in self.bid_prices
+                else f"    {price_level}      "
+            )
+            bid_line += (
+                " "
+                if len(str(price_level)) == 4 and price_level in self.bid_prices
+                else ""
+            )
+            ask_line = (
+                "" if price_level not in self.ask_prices else f"     {price_level}"
+            )
+            volume = (
+                self.ask_volumes[self.ask_prices.index(price_level)]
+                if price_level in self.ask_prices
+                else self.bid_volumes[self.bid_prices.index(price_level)]
+            )
+            volume_line = f"  {volume}  "
+            volume_line += " " if len(str(volume)) == 1 else ""
+
+            lines.append(f"{bid_line} | {volume_line} | {ask_line}\n")
+
+        spread = self.spread
+        mid_price = self.mid_price
+        vwap = self.vwap
+        lines.append(f"Spread: {spread}\n")
+        lines.append(f"Mid Price: {mid_price}\n")
+        lines.append(
+            f"VWAP_BID: {vwap[0]:.2f}, VWAP_ASK: {vwap[1]:.2f}, VWAP_MID: {vwap[2]:.2f}\n"
+        )
+
+        return "".join(lines)
+
+    def update(self, order):
+        if order.quantity > 0:  # Buy order
+            if order.price in self.ask_prices:
+                index = self.ask_prices.index(order.price)
+                if self.ask_volumes[index] > order.quantity:
+                    self.ask_volumes[index] -= order.quantity
+                else:
+                    self.ask_prices.pop(index)
+                    self.ask_volumes.pop(index)
+            else:
+                if order.price in self.bid_prices:
+                    index = self.bid_prices.index(order.price)
+                    self.bid_volumes[index] += order.quantity
+                else:
+                    self.bid_prices.append(order.price)
+                    self.bid_volumes.append(order.quantity)
+        else:  # Sell order
+            if order.price in self.bid_prices:
+                index = self.bid_prices.index(order.price)
+                if self.bid_volumes[index] > abs(order.quantity):
+                    self.bid_volumes[index] -= abs(order.quantity)
+                else:
+                    self.bid_prices.pop(index)
+                    self.bid_volumes.pop(index)
+            else:
+                if order.price in self.ask_prices:
+                    index = self.ask_prices.index(order.price)
+                    self.ask_volumes[index] += abs(order.quantity)
+                else:
+                    self.ask_prices.append(order.price)
+                    self.ask_volumes.append(abs(order.quantity))
+
+        # Sort the order book by price
+        self.sell_orders = sorted(zip(self.ask_prices, self.ask_volumes))
+        self.buy_orders = sorted(zip(self.bid_prices, self.bid_volumes), reverse=True)
+
+        self.ask_prices = [order[0] for order in self.sell_orders]
+        self.ask_volumes = [order[1] for order in self.sell_orders]
+        self.bid_prices = [order[0] for order in self.buy_orders]
+        self.bid_volumes = [order[1] for order in self.buy_orders]
+
+
+class PositionBook:
+    def __init__(self, pos_limit):
+        self.pos_limit = pos_limit
+        self.positions = []
+        self.tot_position = 0
+
+    @property
+    def room_to_buy(self):
+        return self.pos_limit - self.tot_position
+
+    @property
+    def room_to_sell(self):
+        return self.pos_limit + self.tot_position
+
+    def add_pos(self, order, timestamp):
+        self.tot_position += order.quantity
+        self.positions.append([order.price, order.quantity, timestamp])
+
+    def _update_tot_position(self):
+        self.tot_position = sum([pos[1] for pos in self.positions])
+
+    def liquidate_pos(self, qty, timestamp):
+        for pos in self.positions:
+            if pos[2] == timestamp:
+                pos[1] = abs(pos[1]) - qty
+                if pos[1] == 0:
+                    self.positions.remove(pos)
+        self._update_tot_position()
+
+    def __repr__(self):
+        line = "POSITION PRICE | VOLUME | TIMESTAMP\n"
+        lines = [line]
+        for pos in self.positions:
+            price = pos[0]
+            volume = pos[1]
+            timestamp = pos[2]
+            price_line = f"     {price}    "
+            price_line += " " if len(str(price)) == 4 else ""
+            volume_line = f"  {volume}  "
+            lines.append(f"{price_line} | {volume_line} |  {timestamp}\n")
+        return "".join(lines)
